29bb60a32dd6af5d03be2fb3f6e9198b706b24de
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2021-03-03 06:53:09 +0700

    setup controller and route

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = Post::where('draft', false)
+            ->orderBy('id', 'desc')
+            ->paginate(5);
+        return view('posts.index', ['posts' => $posts]);
+    }
+
+    public function show($slug)
+    {
+        $post = Post::where([
+            'slug' => $slug,
+            'draft' => false,
+        ])->first();
+        return view('posts.show', ['post' => $post]);
+    }
 }
