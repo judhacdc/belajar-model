3ee645fdc2cd8a81cb82d94b893777db9aaf7bee
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2021-03-03 06:41:02 +0700

    add seeder

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PostSeeder extends Seeder
 {
@@ -13,6 +14,12 @@ class PostSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('posts')->insert([
+            'title' => 'Belajar Model Dengan Laravel',
+            'slug' => ' belajar-model-dengan-laravel',
+            'image' => 'https://via.placeholder.com/640x480.png/00ff77?text=Belajar Model',
+            'content' => 'Belajar dengan Laravel sungguh menyenangkan',
+            'draft' => 0
+        ]);
     }
 }
